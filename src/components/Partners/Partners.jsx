be2ce21d1aad39c31fc61d8eeff1cae974beec05
Vasiliy Perevalov <VPerevalov02@mail.ru>
2022-03-29 18:17:00 +0300

    Сделал секцию "Наши заказчики" и "У вас есть вопросы?"

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,11 +1,20 @@
 import './Partners.scss'
 import React from 'react';
+import geoizolPath from '../../static/Geoizol.png'
+import lionPath from '../../static/Lion.png'
+import mostotrestPath from '../../static/Mostotrest.png'
 
 const Partners = () => {
   return (
-    <div>
-      Partners
-    </div>
+    <section className= "partners">
+      <h3 className='partners__subtitle'>Нам доверяют</h3>
+      <h2 className='partners__title'>Наши заказчики</h2>
+      <div className='partners__images'>
+        <img className='partners__images-image' src={lionPath} alt="Герб со львом" /> 
+        <img className='partners__images-image' src={geoizolPath} alt="Логотип компании Геоизол" /> 
+        <img className='partners__images-image' src={mostotrestPath} alt="Логотип компании Мостотрест" /> 
+      </div>
+    </section>
   );
 }
 
