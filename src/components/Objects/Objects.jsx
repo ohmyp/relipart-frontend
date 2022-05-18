7058325f9f7e1a1cfe03c9556d70fe2905c29ac6
Evgeny Synkov <synkoveugeny@gmail.com>
2022-05-18 09:04:48 +0300

    Увеличение количетсва объектов и фиксы

--- src/components/Objects/Objects.jsx
+++ src/components/Objects/Objects.jsx
@@ -19,13 +19,13 @@ const Objects = ({handlePopup}) => {
             <p className="object__info-text">
               Стенка канала Петровского дока, участок №3, 2019 г.
             </p>
-            <img id='0' className="object-image" src={embankmentPath} alt="" />
+            <img id='0' className="object-image" src={embankmentPath} alt="Изображение объекта 0" />
           </div>
           <div className="object__info" onClick={openPopup}>
             <p className="object__info-text">
             Дворовая территория дома №37Б литер А по ул. Профессора Попова, ЛЭТИ, 2019 г.
             </p>
-            <img id='1' className="object-image" src={brickBuildtPath} alt="" />
+            <img id='1' className="object-image" src={brickBuildtPath} alt="Изображение объекта 1" />
           </div>
           <div className="object-color orange"></div>
         </div>
@@ -35,14 +35,47 @@ const Objects = ({handlePopup}) => {
             <p className="object__info-text">
             Дворовая территория дома №45 по ул. Ленина, 2018 г.
             </p>
-            <img id='2' className="object-image" src={whiteBuildPath} alt="" />
+            <img id='2' className="object-image" src={whiteBuildPath} alt="Изображение объекта 2" />
           </div>
           <div className="object-color blue"></div>
           <div className="object__info" onClick={openPopup}>
             <p className="object__info-text">
             Дворовая территория дома №50 по Каменноостровскому проспекту, 2019 г.
             </p>
-            <img id='3' className="object-image" src={brownBuildingPath} alt="" />
+            <img id='3' className="object-image" src={brownBuildingPath} alt="Изображение объекта 3" />
+          </div>
+        </div>
+      </div>
+      <div className="objects__objects">
+        <div className="objects__objects-top object">
+          <div className="object__info" onClick={openPopup}>
+            <p className="object__info-text">
+            Дноуглубление участка акватории в рамках проектной документации и причал в г. Новая Ладога Волховского муниципального района Ленинградской области", 2020 г.
+            </p>
+            <img id='4' className="object-image" src={embankmentPath} alt="Изображение объекта 4" />
+          </div>
+          <div className="object__info" onClick={openPopup}>
+            <p className="object__info-text">
+            Корректура проектной документации «Благоустройство наб. Достоевского с частичным берегоукреплением», г.Старая Русса, наб. Достоевского, 2021 г
+            </p>
+            <img id='5' className="object-image" src={brickBuildtPath} alt="Изображение объекта 5" />
+          </div>
+          <div className="object-color orange"></div>
+        </div>
+
+        <div className="objects__objects-bottom object">
+          <div className="object__info" onClick={openPopup}>
+            <p className="object__info-text">
+            Консультационные услуги по объекту "Проектная документация по сохранению объекта культурного наследия «Реальное училище», г. Кронштадт, ул. Андреевская, д.7, 2021г.
+            </p>
+            <img id='6' className="object-image" src={whiteBuildPath} alt="Изображение объекта 6" />
+          </div>
+          <div className="object-color blue"></div>
+          <div className="object__info" onClick={openPopup}>
+            <p className="object__info-text">
+            Корректировка раздела ПОС для реконструкции с элементами реставрации с приспособлением к современному использованию объекта федерального значения «Форт «Кроншлот», I этап, г. Кронштадт, 2022 г.
+            </p>
+            <img id='7' className="object-image" src={brownBuildingPath} alt="Изображение объекта 7" />
           </div>
         </div>
       </div>
