1f9c8d10b497f92031ee646cd70e45a1addcc0c7
Synkov2102 <synkoveugeny@gmail.com>
2022-05-19 13:36:55 +0300

    Сделан файл для управления контентом блока объекты

--- src/components/Objects/Objects.jsx
+++ src/components/Objects/Objects.jsx
@@ -1,81 +1,101 @@
-import "./Objects.scss";
-import React from "react";
-import embankmentPath from "../../static/objects/embankment.png";
-import brickBuildtPath from "../../static/objects/brick_building.png";
-import whiteBuildPath from "../../static/objects/white_building.png";
-import brownBuildingPath from "../../static/objects/brown_building.png";
-const Objects = ({handlePopup}) => {
-    function openPopup (e) { 
-        console.log(e.target)
-        handlePopup(e.target.id)
-    }
+import './Objects.scss';
+import React from 'react';
 
-    return (
+const Objects = ({ handlePopup, objectsData }) => {
+  function openPopup(e) {
+    console.log(e.target);
+    handlePopup(e.target.id);
+  }
+  return (
     <section className="objects">
       <h2 className="objects__title">Наши объекты</h2>
       <div className="objects__objects">
         <div className="objects__objects-top object">
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-              Стенка канала Петровского дока, участок №3, 2019 г.
-            </p>
-            <img id='0' className="object-image" src={embankmentPath} alt="Изображение объекта 0" />
+            <p className="object__info-text">{objectsData[0].title}</p>
+            <img
+              id="0"
+              className="object-image"
+              src={objectsData[0].afterImg}
+              alt="Изображение объекта 0"
+            />
           </div>
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Дворовая территория дома №37Б литер А по ул. Профессора Попова, ЛЭТИ, 2019 г.
-            </p>
-            <img id='1' className="object-image" src={brickBuildtPath} alt="Изображение объекта 1" />
+            <p className="object__info-text">{objectsData[1].title}</p>
+            <img
+              id="1"
+              className="object-image"
+              src={objectsData[1].afterImg}
+              alt="Изображение объекта 1"
+            />
           </div>
           <div className="object-color orange"></div>
         </div>
 
         <div className="objects__objects-bottom object">
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Дворовая территория дома №45 по ул. Ленина, 2018 г.
-            </p>
-            <img id='2' className="object-image" src={whiteBuildPath} alt="Изображение объекта 2" />
+            <p className="object__info-text">{objectsData[2].title}</p>
+            <img
+              id="2"
+              className="object-image"
+              src={objectsData[2].afterImg}
+              alt="Изображение объекта 2"
+            />
           </div>
           <div className="object-color blue"></div>
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Дворовая территория дома №50 по Каменноостровскому проспекту, 2019 г.
-            </p>
-            <img id='3' className="object-image" src={brownBuildingPath} alt="Изображение объекта 3" />
+            <p className="object__info-text">{objectsData[3].title}</p>
+            <img
+              id="3"
+              className="object-image"
+              src={objectsData[3].afterImg}
+              alt="Изображение объекта 3"
+            />
           </div>
         </div>
       </div>
       <div className="objects__objects">
         <div className="objects__objects-top object">
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Дноуглубление участка акватории в рамках проектной документации и причал в г. Новая Ладога Волховского муниципального района Ленинградской области", 2020 г.
-            </p>
-            <img id='4' className="object-image" src={embankmentPath} alt="Изображение объекта 4" />
+            <p className="object__info-text">{objectsData[4].title}</p>
+            <img
+              id="4"
+              className="object-image"
+              src={objectsData[4].afterImg}
+              alt="Изображение объекта 4"
+            />
           </div>
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Корректура проектной документации «Благоустройство наб. Достоевского с частичным берегоукреплением», г.Старая Русса, наб. Достоевского, 2021 г
-            </p>
-            <img id='5' className="object-image" src={brickBuildtPath} alt="Изображение объекта 5" />
+            <p className="object__info-text">{objectsData[5].title}</p>
+            <img
+              id="5"
+              className="object-image"
+              src={objectsData[5].afterImg}
+              alt="Изображение объекта 5"
+            />
           </div>
           <div className="object-color orange"></div>
         </div>
 
         <div className="objects__objects-bottom object">
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Консультационные услуги по объекту "Проектная документация по сохранению объекта культурного наследия «Реальное училище», г. Кронштадт, ул. Андреевская, д.7, 2021г.
-            </p>
-            <img id='6' className="object-image" src={whiteBuildPath} alt="Изображение объекта 6" />
+            <p className="object__info-text">{objectsData[6].title}</p>
+            <img
+              id="6"
+              className="object-image"
+              src={objectsData[6].afterImg}
+              alt="Изображение объекта 6"
+            />
           </div>
           <div className="object-color blue"></div>
           <div className="object__info" onClick={openPopup}>
-            <p className="object__info-text">
-            Корректировка раздела ПОС для реконструкции с элементами реставрации с приспособлением к современному использованию объекта федерального значения «Форт «Кроншлот», I этап, г. Кронштадт, 2022 г.
-            </p>
-            <img id='7' className="object-image" src={brownBuildingPath} alt="Изображение объекта 7" />
+            <p className="object__info-text">{objectsData[7].title}</p>
+            <img
+              id="7"
+              className="object-image"
+              src={objectsData[7].afterImg}
+              alt="Изображение объекта 7"
+            />
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,115 +1,124 @@
-import React from "react";
+import React from 'react';
 import {
-    Header,
-    Footer,
-    Services,
-    Questions,
-    Objects,
-    Partners,
-    CallToAction,
-    MainPicture,
-    ObjectsPopup,
-} from "../components/components";
-
-import beforeImg1 from "../static/objects/embankment.png";
-import afterImg1 from "../static/objects/embankment.png";
-
-import beforeImg2 from "../static/objects/brick_building.png";
-import afterImg2 from "../static/objects/brick_building.png";
-
-import beforeImg3 from "../static/objects/white_building.png";
-import afterImg3 from "../static/objects/white_building.png";
-
-import beforeImg4 from "../static/objects/brown_building.png";
-import afterImg4 from "../static/objects/brown_building.png";
-
-import beforeImg5 from "../static/objects/embankment.png";
-import afterImg5 from "../static/objects/embankment.png";
-
-import beforeImg6 from "../static/objects/brick_building.png";
-import afterImg6 from "../static/objects/brick_building.png";
-
-import beforeImg7 from "../static/objects/white_building.png";
-import afterImg7 from "../static/objects/white_building.png";
-
-import beforeImg8 from "../static/objects/brown_building.png";
-import afterImg8 from "../static/objects/brown_building.png";
+  Header,
+  Footer,
+  Services,
+  Questions,
+  Objects,
+  Partners,
+  CallToAction,
+  MainPicture,
+  ObjectsPopup,
+} from '../components/components';
+import objects from '../for_redaction/objects';
 
 const Home = () => {
-    const [selectedObject, setSelectedObject] = React.useState({});
-    const [isObjectsPopupOpen, SetIsObjectsPopupOpen] = React.useState(false);
+  const objectsData = [
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object0.text,
+      title: objects.object0.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object1.text,
+      title: objects.object1.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object2.text,
+      title: objects.object2.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object3.text,
+      title: objects.object3.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object4.text,
+      title: objects.object4.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object5.text,
+      title: objects.object5.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object6.text,
+      title: objects.object6.title
+    },
+    {
+      beforeImg: '',
+      afterImg: '',
+      text: objects.object7.text,
+      title: objects.object7.title
+    },
+  ];
 
-    const objectsData = [
-        {
-            beforeImg: beforeImg1,
-            afterImg: afterImg1,
-            text: "Консультационные услуги по первоочередным противоаварийным мероприятиям участка стенки набережной по заказу ООО «МИДАС» для нужд СПб ГБУ «Мостотрест» и Администрации Кронштадтского района. В рамках консультационных услуг было проведено дополнительное обследование аварийного участка стенки набережной канала Петровского дока, в результате которого выявилось нарушение каменной кладки из бутового заполнения тела стенки набережной. Было предложено провести инъектирование на глубину сооружения надводной части (2,5 м) с шагом 0,6 м для восстановления несущей способности каменной кладки. Также разработана система технических мероприятий, направленных на устранение последствий вывала каменной кладки на аварийном участке пломбированием. Проект согласован с КГИОП. Работы выполнены с понтона в 2020 г.",
-        },
-        {
-            beforeImg: beforeImg2,
-            afterImg: afterImg2,
-            text: "По заказу Санкт-Петербургского государственного электротехнического университета «ЛЭТИ» им. В.И. Ульянова (Ленина) разработан проект по устройству плаца в асфальтобетонном покрытии со вспомогательными площадками для отработки техники построения студентами военной кафедры университета. Проектная документация согласована с КГА и Комитетом по благоустройству.",
-        },
-        {
-            beforeImg: beforeImg3,
-            afterImg: afterImg3,
-            text: "Проект благоустройства элементов дворовой территории по заказу МАМОМО «Чкаловское»: замена асфальтобетонного покрытия на мощение тротуарной плиткой; предусмотрено устройство водоприемников под водосточные трубы, устройство гидроизоляции по краю мощения, регулировка крышек коверов, выравнивание бортового камня вдоль ограждения. Документация согласована с КГА. Работы выполнены в 2019 г.",
-        },
-        {
-            beforeImg: beforeImg4,
-            afterImg: afterImg4,
-            text: "Проект благоустройства элементов дворовой территории по заказу МАМОМО «Чкаловское»: замена асфальтобетонного покрытия на мощение тротуарной плиткой; предусмотрено устройство водоприемных лотков под водосточные трубы, устройство гидроизоляции по краю мощения, регулировка крышек колодцев, установка облегченного бортового камня. Документация согласована с КГА. Работы выполнены в 2020 г.",
-        },
-        {
-            beforeImg: beforeImg5,
-            afterImg: afterImg5,
-            text: 'Для нужд ГКУ «Управление строительства Ленинградской области» и ГУ МЧС по заказу ООО «Монтаж-Строй Северо-Запад» в рамках корректировки проектной документации, разработанной ООО «БАЛТПРОЕКТ», выполнены проектно-изыскательские работы по дноуглублению с устройством слипа, включающего в себя гидротехнические и конструктивные решения укрепления подводного откоса, мероприятия по охране окружающей среды, технологические решения по производству работ дноуглубления. Проектная документация согласована с Росрыболовством. Работы выполнены в 2021 г.',
-        },
-        {
-            beforeImg: beforeImg6,
-            afterImg: afterImg6,
-            text: 'Для нужд администрации г. Старая Русса по заказу ООО «НТЦ «Северо-Запад Проект» в рамках корректуры проектной документации разработан раздел «Схема планировочной организации земельного участка» в составе краткой пояснительной записки, решений по благоустройству с элементами озеленения (дендроплан), разбивочно-посадочного чертежа, чертежа по вертикальной планировки с картограммой земляных масс, ведомости объемов работ по озеленению. Работы выполнены в 2021 г.',
-        },
-        {
-            beforeImg: beforeImg7,
-            afterImg: afterImg7,
-            text: 'Для нужд администрации Кронштадтского района по заказу ООО «МИДАС» выполнена проверка проектной документации с корректировкой ведомостей объемов работ на ремонтно-реставрационные работы с приспособлением для современного использования здания школы.',
-        },
-        {
-            beforeImg: beforeImg8,
-            afterImg: afterImg8,
-            text: 'Для нужд Минобороны РФ по заказу ООО «ГЕОИЗОЛ Проект» выполнена корректировка раздела «Проект организации строительства» в части описания технологических процессов с разработкой чертежей по Северной куртине. Определена технология работ по бетонированию Северной куртины и берегоукреплению 257 п.м, определена технология подъёма посторонних предметов со дна акватории, разработаны чертежи по СВСиУ',
-        },
-    ];
-
-    function openObjectsPopup(objectId) {
-        document.querySelector('.body').classList.add('body_non-scroll')
-        SetIsObjectsPopupOpen(true);
-        setSelectedObject(objectsData[objectId]);
+  const [selectedObject, setSelectedObject] = React.useState({});
+  const [objectsForRender, setObjectsForRender] = React.useState([
+    ...objectsData,
+  ]);
+  const [isObjectsPopupOpen, SetIsObjectsPopupOpen] = React.useState(false);
+  React.useEffect(() => {
+    // Тут происходит динамический импорт картинок и загрузка его в массив объектов
+    let buffer = [];
+    for (let i = 0; i < objectsData.length; i++) {
+      buffer.push(
+        import(`../static/objects/${objects[`object${i}`].pictureBefore}`)
+          .then((module) => {
+            objectsData[i].beforeImg = module.default;
+            return import(
+              `../static/objects/${objects[`object${i}`].pictureAfter}`
+            );
+          })
+          .then((module) => {
+            objectsData[i].afterImg = module.default;
+          })
+          .catch((err) => console.log(err))
+      );
     }
+    Promise.all([...buffer])
+        .then(()=> setObjectsForRender(objectsData));
+  }, []);
 
-    function closePopup() {
-        document.querySelector('.body').classList.remove('body_non-scroll')
-        SetIsObjectsPopupOpen(false);
-    }
+  function openObjectsPopup(objectId) {
+    document.querySelector('.body').classList.add('body_non-scroll');
+    SetIsObjectsPopupOpen(true);
+    setSelectedObject(objectsForRender[objectId]);
+  }
 
-    return (
-        <>
-            <Header/>
-            <MainPicture/>
-            <Services/>
-            <Questions/>
-            <Objects handlePopup={openObjectsPopup}/>
-            <Partners/>
-            <CallToAction/>
-            <Footer/>
-            <ObjectsPopup
-                object={selectedObject}
-                onClose={closePopup}
-                isOpen={isObjectsPopupOpen}
-            />
-        </>
-    );
+  function closePopup() {
+    document.querySelector('.body').classList.remove('body_non-scroll');
+    SetIsObjectsPopupOpen(false);
+  }
+
+  return (
+    <>
+      <Header />
+      <MainPicture />
+      <Services />
+      <Questions />
+      <Objects handlePopup={openObjectsPopup} objectsData={objectsForRender} />
+      <Partners />
+      <CallToAction />
+      <Footer />
+      <ObjectsPopup
+        object={selectedObject}
+        onClose={closePopup}
+        isOpen={isObjectsPopupOpen}
+      />
+    </>
+  );
 };
 
 export default Home;
